d19087f486136156e57cf41ef55a4b46dcb16689
Noadi NONO <adingra721@gmail.com>
2024-05-23 15:49:24 +0000

    initial commit

---
+++ src/main/java/com/formation/ansible_boot/AnsibleBootApplication.java
@@ -0,0 +1,25 @@
+package com.formation.ansible_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AnsibleBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleBootApplication.class, args);
+		
+		
+	}
+	
+	@GetMapping(value = "hello")
+	public String hello() {
+		return "<h2> Bonjour spring v5  </h2>" ;
+	}
+
+	
+	
+}
---
+++ src/test/java/com/formation/ansible_boot/AnsibleBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.ansible_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
